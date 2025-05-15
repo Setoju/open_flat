1bee3d2bb15fed12816eb29c3ae1dad4c8e4df26
Setoju <setoju48@gmail.com>
2025-05-15 18:24:19 +0300

    Implement apartment listing creation.

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -2,4 +2,23 @@ class ApartmentsController < ApplicationController
   def index
     @apartments = Apartment.all
   end
+
+  def new
+    @apartment = Apartment.new
+  end
+
+  def create
+    @apartment = Apartment.new(apartment_params)
+    if @apartment.save
+      redirect_to apartments_path, notice: "Apartment listing created successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def apartment_params
+    params.require(:apartment).permit(:title, :price, :address, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  resources :apartments, only: [:index]
+  resources :apartments, only: [:index, :new, :create]
   root "apartments#index"
 end
